4376437fe6e6a85cab94f9188e09fbbc27059fd6
jgarmendia <j.garmendia.serey@gmail.com>
2019-06-10 17:35:10 -0400

    hoot fix

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,24 @@
-  /*****************************************************************************
-   *
-   * Service worker
-   *
-   ****************************************************************************/
+/*****************************************************************************
+ *
+ * Service worker
+ *
+ ****************************************************************************/
 
 // nombre del cache actual
 // IMPORTANTE !!!  (se debe cambiar el nombre por cada cambio)
 //var cacheName = 'dados-a-03';
-const CACHE_VERSION = "dados-v4a";
+const CACHE_VERSION = "v4b";
 const CACHE_NAME = `${registration.scope}!${CACHE_VERSION}`;
 
 // lista de archivos necesarios para la shell app (index, js, imagenes,css, etc)
 // para github pages, se mepieza con el nombre del repositorio
 var filesToCache = [
-    '/dados/',
-    '/dados/index.html',
-    '/dados/app.js',
-    '/dados/style.css',
-    '/dados/media/dados_sprite01.png',
-    '/dados/media/midi_lst.mp3'
+  "/dados/",
+  "/dados/index.html",
+  "/dados/app.js",
+  "/dados/style.css",
+  "/dados/media/dados_sprite01.png",
+  "/dados/media/midi_lst.mp3"
 ];
 
 /*
@@ -37,11 +37,11 @@ var filesToCache = [
 // Instalación
 // se abre cache y se da un nombre con caches.open()
 // cache.addAll() toma una lista de URL, las obtiene del servidor y agrega la respuesta al cache.
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
+self.addEventListener("install", function(e) {
+  console.log("[ServiceWorker] Install");
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
+      console.log("[ServiceWorker] Caching app shell");
       return cache.addAll(filesToCache);
     })
   );
@@ -51,20 +51,23 @@ self.addEventListener('install', function(e) {
 // se inicia, cuando se inicia el service worker.
 // se actualiza el cache cada vez que se cambie el cacheName
 self.addEventListener("activate", event => {
-  console.log('[ServiceWorker] Activate');
-  const currentCaches = CACHE_NAME;
+  console.log("[ServiceWorker] Activate");
   event.waitUntil(
     caches
       .keys()
       .then(cacheNames => {
-        return cacheNames.filter(
-          cacheName => !currentCaches.includes(cacheName)
-        );
+        return cacheNames.filter(cacheName => {
+          // Find the caches that belong to this scope, but don't match CACHE_NAME.
+          return (
+            cacheName.startsWith(`${registration.scope}!`) &&
+            cacheName !== CACHE_NAME
+          );
+        });
       })
       .then(cachesToDelete => {
         return Promise.all(
           cachesToDelete.map(cacheToDelete => {
-            console.log('[ServiceWorker] Removing old cache', cacheToDelete);
+            console.log("[ServiceWorker] Removing old cache", cacheToDelete);
             return caches.delete(cacheToDelete);
           })
         );
@@ -77,11 +80,11 @@ self.addEventListener("activate", event => {
 // intercepta las solicitudes de la PWA para controlarlas con el service worker.
 // caches.match() responde con la versión almacenada del cache o usa fetch para obtener una copia de la red.
 // e.respondWith() devuelve la response.
-  self.addEventListener('fetch', function(e) {
-    console.log('[ServiceWorker] Fetch', e.request.url);
-    e.respondWith(
-      caches.match(e.request).then(function(response) {
-        return response || fetch(e.request);
-      })
-    );
-  });
+self.addEventListener("fetch", function(e) {
+  console.log("[ServiceWorker] Fetch", e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
